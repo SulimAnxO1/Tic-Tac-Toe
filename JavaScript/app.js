01a11319519a85d812bd03f12c917bc0e1e04386
SulimAnxO1 <mxsulaimansufian@gmail.com>
2025-10-05 04:40:47 +0600

    Game Complete but need UI changes and improvement

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,7 +1,11 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;
+let count = 0; //To Track Draw
 
 const winPatterns = [
   [0, 1, 2],
@@ -14,15 +18,77 @@ const winPatterns = [
   [6, 7, 8],
 ];
 
+// Reset Game
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+// Game Logic
 boxes.forEach((box) => {
-  box.addEventListener("clicked", () => {
-    console.log("Value Added");
+  box.addEventListener("click", () => {
     if (turnO) {
-      box.innerText = "O";
+      box.innerText = "O"; // Player O
       turnO = false;
     } else {
-      box.innerText = "X";
+      box.innerText = "X"; // Player X
       turnO = true;
+    }
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+    if (count === 9 && !isWinner) {
+      gameDraw();
     }
   });
 });
+
+const gameDraw = () => {
+  msg.innerText = `Game Draw! Try again.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+// Winning / Draw State
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+// New Game
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+// Winner Screen
+const showWinner = (winner) => {
+  msg.innerText = `Player ${winner} Wins`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
+
+// Winner logic
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
+
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
+    }
+  }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
